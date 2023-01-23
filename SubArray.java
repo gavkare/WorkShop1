43388cf9b6f048a3d52c14a90db74be98e908257
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-01-23 09:40:36 +0530

    Add files via upload

---
+++ SubArray.java
@@ -0,0 +1,15 @@
+package WorkShop;
+
+import java.util.Arrays;
+
+public class SubArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 6, 3, 4, 5 ,6 ,8};
+        int last = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+        System.out.println(Arrays.toString(arr));
+    }
+}
